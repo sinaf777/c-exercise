fd2e6c842c22956064d93e7ff207a8a324d45dc9
Sinaf <166249512+sinaf777@users.noreply.github.com>
2025-04-25 23:53:56 +0300

    Add files via upload

---
+++ lab 1.cpp
@@ -0,0 +1,38 @@
+/*#include <iostream> 
+using namespace std;
+int add(int x, int y){
+	return x + y;
+}
+int sub (int x, int y){
+	return x - y;
+}
+int main(){
+	int x = 3, y = 4;
+	cout<<add(x,y)<<endl;
+	cout<<sub(x,y)<<endl;
+	return 0;
+}*/
+
+
+/*#include <iostream>
+#include <cmath>
+using namespace std;
+double sphare_vol(double r) {
+    return (4.0 / 3.0) * M_PI * pow(r, 3); 
+}
+double cyl_vol(double r, double h) {
+    return M_PI * pow(r, 2) * h;
+}
+double cone_vol(double r, double h) {
+    return (1.0 / 3.0) * M_PI * pow(r, 2) * h;  
+}
+int main() {
+    double r = 8, h = 9;
+    cout << "Sphere volume = " << sphare_vol(r) << endl;
+    cout << "Cylinder volume = " << cyl_vol(r, h) << endl;
+    cout << "Cone volume = " << cone_vol(r, h) << endl;
+    return 0;
+}*/
+
+
+
